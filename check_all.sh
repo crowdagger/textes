36918ae3f8e12a08b6ca486f6c7d5c77b1590132
Lizzie Crowdagger <lizzie@crowdagger.fr>
2017-03-20 22:59:37 +0100

    Ajout de scripts pour générer et epubchecker tous les textes

---
+++ check_all.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+echo "Noir & blanc..."
+epubcheck blanc-noir/blanc-noir.epub
+
+echo
+echo "Pas tout à fait des hommes..."
+epubcheck ptafdh/endr.epub
+
+echo
+echo "Sorcières & zombies..."
+cd recueils/sorcieres_zombies/
+epubcheck sorcieres_zombies.epub
+epubcheck route_de_nuit.epub
+epubcheck creatures_de_reve.epub
+epubcheck sortir_du_cercueil.epub
+epubcheck mine_de_deterres.epub
+cd ../../
+
+echo
+echo "Nouvelles..."
+cd nouvelles
+epubcheck pirate.epub
+epubcheck troll.epub
+epubcheck space_labrys.epub
+epubcheck mauvais_genre.epub
+epubcheck pulmonaire/pulmonaire.epub
+epubcheck revolution/revolution.epub
+cd ..
+
+echo
+echo "Nouvelles Hell B*tches..."
+cd hell_butches/
+epubcheck sigkill.epub
+epubcheck morgue.epub
+epubcheck soleil.epub
+epubcheck acab.epub
+cd ..
+
+echo
+echo "Textes non finalisés..."
+epubcheck enieme_prophetie/enieme_prophetie.epub
+epubcheck from_rust_till_dawn/rust_till_dawn.epub
---
+++ gen_all.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+echo "Noir & blanc..."
+crowbook blanc-noir/blanc-noir.book
+
+echo
+echo "Pas tout à fait des hommes..."
+crowbook ptafdh/ptafdh.book
+
+echo
+echo "Sorcières & zombies..."
+cd recueils/sorcieres_zombies/
+crowbook sorcieres_zombies.book
+crowbook route_de_nuit.book
+crowbook creatures_de_reve.book
+crowbook -s 03_sortir_du_cercueil.md
+crowbook -s 04_mine_deterres.md
+cd ../../
+
+echo
+echo "Nouvelles..."
+cd nouvelles
+crowbook -s pirate.md
+crowbook -s troll.md
+crowbook -s space_labrys.md
+crowbook -s mauvais_genre.md
+crowbook pulmonaire/pulmonaire.book
+crowbook revolution/revolution.book
+cd ..
+
+echo
+echo "Nouvelles Hell B*tches..."
+cd hell_butches/
+crowbook -s sigkill.md
+crowbook -s morgue.md
+crowbook -s soleil.md
+crowbook -s acab.md
+cd ..
+
+echo
+echo "Textes non finalisés..."
+crowbook enieme_prophetie/enieme_prophetie.book
+crowbook from_rust_till_dawn/rust_till_dawn.book
